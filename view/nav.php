1d1d45aba635f61f746a918c8976bae31f0b9925
Vannah Ranaivoson <vgranaivoson@gmail.com>
2020-09-24 16:33:57 +0200

    modif logo dawan

--- view/nav.php
+++ view/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-light bg-light">
-  <h1><img src="https://www.dawan.fr/images/institutionnel-logo.png" alt=""></h1>
+  <h1><img src="assets/img/logoalumni.png" alt=""></h1>
   <div>
   <button id='btn-sign' type="button" class="btn btn-primary">Inscription</button>
   <button id="btn-connexion" type="button" class="btn btn-primary">Connexion</button>
