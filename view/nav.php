c463e97481b42336444916781ca29388fd394baa
MagloireIT <magloiredjomolite1@gmail.com>
2020-09-25 10:41:55 +0200

    enlevage du padding-left

--- view/nav.php
+++ view/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-dark bg-dark">
-  <h1><img src="assets/img/logoalumni.png" class="img-fluid" alt=""></h1>
-  <div>
+  <h1 class=""><img src="assets/img/logoalumni.png" class="img-fluid" alt=""></h1>
+  <div class="">
   <button id='btn-sign' type="button" class="btn btn-primary">Inscription</button>
   <button id="btn-connexion" type="button" class="btn btn-primary">Connexion</button>
   </div>
