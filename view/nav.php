db5abdfe03c183212dcae666647d54ae3b12fd5c
loic <loicmetringer@gmail.com>
2020-09-25 12:24:49 +0200

    button margin -2em pour centrer le logo

--- view/nav.php
+++ view/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-dark bg-dark">
   <h1 class="pl-md-5"><img src="assets/img/logoalumni.png" class="img-fluid pl-md-5 ml-md-5" alt=""></h1>
-  <div class="connexion">
+  <div class="connexion" style="margin-left: -3em;">
   <button id='btn-sign' type="button" class="btn btn-primary">Inscription</button>
   <button id="btn-connexion" type="button" class="btn btn-primary">Connexion</button>
   </div>
