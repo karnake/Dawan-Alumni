6fbd4c6f688e17ba9cb4b53f1590951e1d9a7ab2
loic <loicmetringer@gmail.com>
2020-09-25 12:20:37 +0200

    h1 + igm padding et margin responsive pour centrer le logo

--- view/nav.php
+++ view/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark bg-dark">
-  <h1 class=""><img src="assets/img/logoalumni.png" class="img-fluid" alt=""></h1>
+  <h1 class="pl-md-5"><img src="assets/img/logoalumni.png" class="img-fluid pl-md-5 ml-md-5" alt=""></h1>
   <div class="connexion">
   <button id='btn-sign' type="button" class="btn btn-primary">Inscription</button>
   <button id="btn-connexion" type="button" class="btn btn-primary">Connexion</button>
