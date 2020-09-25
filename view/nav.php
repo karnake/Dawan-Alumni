7c753faf9be49bb495a564aab741e6c13fe19362
MagloireIT <magloiredjomolite1@gmail.com>
2020-09-25 09:52:50 +0200

    centrage logo + responsive

--- view/nav.php
+++ view/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark bg-dark">
-  <h1><img src="assets/img/logoalumni.png" alt=""></h1>
+  <h1><img src="assets/img/logoalumni.png" class="img-fluid" alt=""></h1>
   <div>
   <button id='btn-sign' type="button" class="btn btn-primary">Inscription</button>
   <button id="btn-connexion" type="button" class="btn btn-primary">Connexion</button>
