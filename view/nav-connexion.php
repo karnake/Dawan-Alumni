38886e0f15cca751bc8697ffe040412efdf06628
Vannah Ranaivoson <vgranaivoson@gmail.com>
2020-09-25 16:26:04 +0200

    header connexion.php view

--- view/nav-connexion.php
+++ view/nav-connexion.php
@@ -2,8 +2,18 @@
   <h1><img src="../assets/img/logoalumni.png" class="img-fluid" alt=""></h1>
 
   <div>
-  <button id='btn-sign' type="button" class="btn btn-primary">Accueil</button>
-  <button id="btn-connexion" type="button" class="btn btn-primary">Forum</button>
-  <a href="#"><img src="../assets/img/logout.svg" alt="bouton déconnexion" width ="35px" ></a>
+    <div class="col-m-8">
+    <h1 class="pl-md-5"><img src="assets/img/logoalumni.png" class="img-fluid pl-md-5 ml-md-5" alt=""></h1>
+    </div>
+    <div>
+      <p>Nom</p>
+      <p>Prenom</p>
+      <img src="" alt="">
+    </div>
+
+
+  <!-- <button id='btn-home' type="button" class="btn btn-primary">Accueil</button>
+  <button id="btn-forum" type="button" class="btn btn-primary">Forum</button> -->
+  <!-- <a href="#"><img src="../assets/img/logout.svg" alt="bouton déconnexion" width ="35px" ></a> -->
   </div>
 </nav>
