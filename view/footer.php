41aafbee4dc45b313fbe1d72bea4013a731615a2
loic <loicmetringer@gmail.com>
2020-09-25 11:18:38 +0200

    lien dawan blanc

--- view/footer.php
+++ view/footer.php
@@ -1,3 +1,3 @@
 <div class="footer bg-dark mb-0">
-    <p class="mb-0 text-center text-white"><a href="https://www.dawan.fr/">DAWAN</a>&copy;</p>
+    <p class="mb-0 text-center text-white"><a href="https://www.dawan.fr/" style="text-decoration: none; color: white;">DAWAN</a>&copy;</p>
 </div>
