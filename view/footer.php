a532e4f6133527fc57c542bdbf659f68d8be22c5
loic <loicmetringer@gmail.com>
2020-09-25 09:14:57 +0200

    lien dawan email

--- view/footer.php
+++ view/footer.php
@@ -1,3 +1,3 @@
 <div class="footer bg-dark mb-0">
-    <p class="mb-0 text-center text-white">&copy;</p>
+    <p class="mb-0 text-center text-white"> <a href="https://www.dawan.fr/">www.dawan.fr</a>&copy;</p>
 </div>
