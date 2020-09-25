2bb00029d8fa6b98ee4ad202b9accb7a06ee0796
MagloireIT <magloiredjomolite1@gmail.com>
2020-09-25 10:04:37 +0200

    changement de couleur + responsive fixed

--- view/nav.php
+++ view/nav.php
@@ -1,9 +1,5 @@
 <nav class="navbar navbar-dark bg-dark">
-<<<<<<< HEAD
   <h1><img src="assets/img/logoalumni.png" class="img-fluid" alt=""></h1>
-=======
-  <h1><img src="assets/img/logoalumni.png" alt=""></h1>
->>>>>>> develop
   <div>
   <button id='btn-sign' type="button" class="btn btn-primary">Inscription</button>
   <button id="btn-connexion" type="button" class="btn btn-primary">Connexion</button>
