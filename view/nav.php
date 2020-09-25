530c5b7f8c11c6353b9fe8d87248180c90ef750b
MagloireIT <magloiredjomolite1@gmail.com>
2020-09-25 10:44:56 +0200

    centrage des boutons

--- view/nav.php
+++ view/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-dark bg-dark">
   <h1 class=""><img src="assets/img/logoalumni.png" class="img-fluid" alt=""></h1>
-  <div class="">
+  <div class="connexion">
   <button id='btn-sign' type="button" class="btn btn-primary">Inscription</button>
   <button id="btn-connexion" type="button" class="btn btn-primary">Connexion</button>
   </div>
