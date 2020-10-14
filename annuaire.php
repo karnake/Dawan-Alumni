d3dfb919270d2b588c573d87c3aad5fa925fb606
Djomolite.Magloire <magloiredjomolite1@gmail.com>
2020-10-14 16:58:16 +0200

    ANNUAIRE

---
+++ annuaire.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/style.css">
+    <title>Annuaire</title>
+</head>
+<body>
+    
+
+<div class="">
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+</div>
+
+
+
+
+<div class="container annuaire">
+
+
+  <h1>Annuaire des étudiants Dawan </h1>
+      <br>
+      <br>
+
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.8103214176567!2d2.316389115902547!3d48.8427566097611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671ccb16540b5%3A0xb94e60afcc950426!2sDAWAN!5e0!3m2!1sfr!2sfr!4v1602682356511!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+</div>
+
+
+<div class="container-fluid section">
+
+  <h2>Ils nous ont fait confiance</h2>
+
+</div>
+
+<div class="container">
+
+     <div class="row " style="text-align:center;">
+         <div class="col-md-4">
+    <img src="assets/img/CIC.png" style="width:170px; " alt="Logo CIC">
+        </div>
+        <div class="col-md-4">
+    <img src="assets/img/audi.png" style="width:170px; " alt="Audi">
+        </div>
+        <div class="col-md-4">
+    <img src="assets/img/total.png" style="width:190px; " alt="TOTAL">
+        </div>
+
+     </div>
+
+</div>
+
+<div class="container">
+
+     <div class="row " style="text-align:center;">
+         <div class="col-md-4">
+    <img src="assets/img/paris.png" style="width:170px;" alt="mairie de paris">
+        </div>
+        <div class="col-md-4">
+    <img src="assets/img/laposte.png" style="width:170px;" alt="laposte">
+        </div>
+        <div class="col-md-4">
+    <img src="assets/img/bnp.jpg" style="width:190px;" alt="bnp">
+        </div>
+
+     </div>
+
+</div>
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+</body>
+</html>
