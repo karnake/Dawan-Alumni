56b2cc33a3169c1f2c19dd7d77ce17c28e7c1a72
loic <loicmetringer@gmail.com>
2020-10-14 15:39:40 +0200

    page cours

---
+++ cours.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dawan Alumni</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+<?php include "/wamp64/www/Dawan-Alumni/view/nav.php"; ?>
+<br><br><br><br><br><br><br><br><br>
+
+
+<div class="container">
+    <div class="row">
+    <div class="col-md-4"> 
+    <button class="btn btn-outline-dark btn-block">HTML</button>
+    </div>
+
+
+    
+    <div class="col-md-4"> 
+    <button class="btn btn-outline-dark btn-block">CSS</button>
+
+    </div>
+
+    
+    <div class="col-md-4"> 
+    <button class="btn btn-outline-dark btn-block">JAVASCRIPT</button>
+
+</div>
+    </div>
+    <br><br><br><br><br>
+<div class="row">
+    <div class="col-md-4"> 
+    <button class="btn btn-outline-dark btn-block">JQUERY</button>
+    </div>
+
+
+    
+    <div class="col-md-4"> 
+    <button class="btn btn-outline-dark btn-block">PHP</button>
+
+    </div>
+
+    
+    <div class="col-md-4"> 
+    <button class="btn btn-outline-dark btn-block">GIT</button>
+
+    </div>
+
+
+
+
+
+</div>
+</div>
+
+
+    <?php include "/wamp64/www/Dawan-Alumni/view/footer.php"; ?>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script type="module" src="assets/js/app.js"></script>
+
+</body>
+
+</html>
