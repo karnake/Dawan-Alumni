3e8e7975b3f3c29250b7a73f6ded8eb19a18ad12
Vannah RANAIVOSON <vgranaivoson@gmail.com>
2020-10-15 17:10:10 +0200

    responsive navBar

--- view/nav-connexion.php
+++ view/nav-connexion.php
@@ -1,12 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-dark">
-  <div class="col-md-4 logo">
-    <a href="accueil.php"><img src="../assets\img\logoalumni.png" class="img-fluid" alt=""></a>
-  </div>
-
-  
-
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav col-md-6">
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <!-- Brand -->
+  <a href="accueil.php"><img src="../assets\img\logoalumni.png" class="img-fluid" alt=""></a>
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" href="accueil.php">Accueil</a>
       </li>
@@ -19,19 +19,18 @@
       <li class="nav-item">
         <a class="nav-link" href="annuaire.php">Annuaire</a>
       </li>
-    </ul>
-  </div>
-  <div class="col-md-2">
-    <ul class="navbar-nav">
+    
+  
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Mon compte
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="reglagecompte.php">Réglages</a>
-          <a class="dropdown-item" href="#">Déconnexion</a>
-        </div>
-      </li>
-    </ul>
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Mon compte
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="reglagecompte.php">Réglages</a>
+        <a class="dropdown-item" href="#">Déconnexion</a>
+      </div>
+    </li>
+   </ul>
   </div>
 </nav>
+
