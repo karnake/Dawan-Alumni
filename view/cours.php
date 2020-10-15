99d2167849dd2b050d16bd6adf6d8fdc5823a5ec
loic <loicmetringer@gmail.com>
2020-10-15 12:13:17 +0200

    modif lien footer cours

--- view/cours.php
+++ view/cours.php
@@ -61,7 +61,7 @@
 </div>
 
 
-    <?php include "/wamp64/www/Dawan-Alumni/view/footer.php"; ?>
+    <?php include "footer.php"; ?>
     <script src="https://code.jquery.com/jquery-3.5.1.js"
         integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
         crossorigin="anonymous"></script>
