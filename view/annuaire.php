a24e63978650ed0208d11b5cc15c14a9d4b6f806
MagloireIT <magloiredjomolite1@gmail.com>
2020-10-15 11:25:47 +0200

    resolve css problem

--- view/annuaire.php
+++ view/annuaire.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="../assets/css/style.css">
     <title>Annuaire</title>
 </head>
 <body>
     
 
 
-<?php include "view/nav-connexion.php"; ?>
+<?php include "nav-connexion.php"; ?>
 
 
 
